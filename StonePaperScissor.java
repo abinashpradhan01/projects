3efdac2bce20d90cb6eeaabdabc90b50a0962fe6
abinashpradhan01 <teamap13@outlook.com>
2024-08-31 02:06:07 +0530

    My third project
    Actually I loved writing this code, though I had to change a lot of stuffs which can be understood from the code itself. Somehow it clicked me that instead of many else if statements, I can combine them together, and I did. But trust me, I loved writing this one the most among the three small projects that I have made.

---
+++ StonePaperScissor.java
@@ -0,0 +1,73 @@
+// Building a game, to play stone paper and scissor
+// Stone : 1
+// Paper : 0
+// Scissor : -1
+// For win = 2 points
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Stone_Paper_Scissor_Game {
+    public static void main(String[] args) {
+        Scanner sh = new Scanner(System.in);
+        int you = 0;
+        String compChoice = "";
+        int cpoints = 0, ypoints = 0;
+
+        System.out.println("STONE----PAPER----SCISSOR");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Choose your response (Stone, paper or Scissor): ");
+            String choice = sh.next();
+
+            if (choice.equalsIgnoreCase("stone")) {
+                you = 1;
+            } else if (choice.equalsIgnoreCase("paper")) {
+                you = 0;
+            } else if (choice.equalsIgnoreCase("scissor")) {
+                you = -1;
+            } else {
+                System.out.println("Invalid input.");
+                System.exit(0);
+            }
+
+            Random rand = new Random();
+            int computer = rand.nextInt(3) - 1;
+
+            if (computer == 1) {
+                compChoice = "Stone";
+            } else if (computer == 0) {
+                compChoice = "Paper";
+            } else {
+                compChoice = "Scissor";
+            }
+
+            System.out.println("You chose: " + choice.toUpperCase());
+            System.out.println("Computer chose: " + compChoice.toUpperCase());
+
+            if (computer == 1 && you == 1 || computer == -1 && you == -1 || computer == 0 && you == 0) {
+                System.out.println("It's a draw.");
+            } else if (computer == 1 && you == -1 || computer == -1 && you == 0 || computer == 0 && you == 1) {
+                System.out.println("You lose, try again...");
+                cpoints = cpoints + 2;
+            } else {
+                System.out.println("You WIN, Congratulations.");
+                ypoints = ypoints + 2;
+            }
+        }
+
+        System.out.print("Final Score: ");
+        System.out.println("You: " + ypoints + " points");
+        System.out.println("Computer: " + cpoints + " points");
+
+        if (ypoints > cpoints) {
+            System.out.printf("Voila! You WIN this game.\nCongratulations and Celebrations.");
+        } else if (ypoints < cpoints) {
+            System.out.printf("Sorry, you lost.\nIt's a matter of luck,\nI'm sure you will win next time.");
+        } else {
+            System.out.println("Unfortunately, it is a draw. Try hard next time.");
+        }
+
+        sh.close();
+    }
+}
